ca903b29eca38dbecc350c73f63f1027f3822d4c
cmarrerodb <cmarrerodb@gmail.com>
2024-05-16 22:55:34 -0400

    20240516_2255 Se inició el dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,121 @@
 @extends('adminlte::page')
 
 @section('content_header')
-    <h1 class="m-0 text-dark">Dashboard</h1>
+    <h1 class="m-0 text-dark">Tablero</h1>
 @stop
 
 @section('content')
     <div class="row">
-        <div class="col-12">
-            <div class="card">
+        <div class="col-xs-12 col-sm-12 col-md-4">
+            <div class="card text-white bg-primary mb-3 mr-3 border-dark shadow-lg rounded">
+                <div class="card-header">HABILITADOS PARA VOTAR</div>
                 <div class="card-body">
-                    <p class="mb-0">You are logged in!</p>
+                    <h5 class="card-title"></h5>
+                    <ul>
+                        <li><h3>Total: 54.471</h3></li>
+                        <li><h3>Movilizados: 27.238</h3></li>
+                        <li><h3>Por Movilizar: 27.233</h3></li>
+                    </ul>
+                    <!-- <h2 class="card-text">45.315</h2> -->
+
                 </div>
-            </div>
+            </div>            
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-4">
+            <div class="card text-white bg-success mb-3 mr-3 border-dark shadow-lg rounded">
+                <div class="card-header">ESTUDIANTES</div>
+                <div class="card-body">
+                    <h5 class="card-title"></h5>
+                    <ul>
+                        <li><h3>Total: 46.511</h3></li>
+                        <li><h3>Movilizados: 23.267</h3></li>
+                        <li><h3>Por Movilizar: 23.244</h3></li>
+                    </ul>                    
+                </div>
+            </div>            
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-4">
+            <div class="card text-white bg-warning text-dark mb-3 mr-3 border-dark shadow-lg rounded">
+                <div class="card-header">TRABAJADORES</div>
+                <div class="card-body">
+                    <h5 class="card-title"></h5>
+                    <ul>
+                        <li><h3>Total: 7.960</h3></li>
+                        <li><h3>Movilizados: 3.971</h3></li>
+                        <li><h3>Por Movilizar: 3.989</h3></li>                    
+                    </ul>
+                </div>
+            </div>            
         </div>
     </div>
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-3">
+            <div class="card text-white bg-info mb-3 mr-3 border-dark shadow-lg rounded">
+                <div class="card-header">ADMINISTRATIVOS</div>
+                <div class="card-body">
+                    <h5 class="card-title"></h5>
+                    <ul>
+                        <li><h3>Total: 1.084</h3></li>
+                        <li><h3>Movilizados: 541</h3></li>
+                        <li><h3>Por Movilizar: 543</h3></li>                    
+                    </ul>
+                </div>
+            </div>            
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-3">
+            <div class="card text-white bg-dark mb-3 mr-3 border-dark shadow-lg rounded">
+                <div class="card-header">DOCENTES</div>
+                <div class="card-body">
+                    <h5 class="card-title"></h5>
+                    <ul>
+                        <li><h3>Total: 3.849</h3></li>
+                        <li><h3>Movilizados: 1.770</h3></li>
+                        <li><h3>Por Movilizar: 1.719</h3></li>                    
+                    </ul>                    
+                </div>
+            </div>            
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-3">
+            <div class="card text-white bg-light text-dark mb-3 mr-3 border-dark shadow-lg rounded">
+                <div class="card-header">OBREROS</div>
+                <div class="card-body">
+                    <h5 class="card-title"></h5>
+                    <ul>
+                        <li><h3>Total: 1.025</h3></li>
+                        <li><h3>Movilizados: 483</h3></li>
+                        <li><h3>Por Movilizar: 542</h3></li>                    
+                    </ul>                       
+                </div>
+            </div>            
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-3">
+            <div class="card bg-secondary text-dark mb-3 mr-3 border-dark shadow-lg rounded">
+                <div class="card-header">PENSIONADOS</div>
+                <div class="card-body">
+                    <h5 class="card-title"></h5>
+                    <ul>
+                        <li><h3>Total: 406</h3></li>
+                        <li><h3>Movilizados: 199</h3></li>
+                        <li><h3>Por Movilizar: 207</h3></li>                    
+                    </ul>   
+                </div>
+            </div>            
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-3">
+            <div class="card text-white bg-danger text-dark mb-3 mr-3 border-dark shadow-lg rounded">
+                <div class="card-header">JUBILADOS</div>
+                <div class="card-body">
+                    <h5 class="card-title"></h5>
+                    <ul>
+                        <li><h3>Total: 1.956</h3></li>
+                        <li><h3>Movilizados: 978</h3></li>
+                        <li><h3>Por Movilizar: 978</h3></li>                    
+                    </ul>  
+                </div>
+            </div>            
+        </div>
+    </div>
+    <hr>
 @stop
